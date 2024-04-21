9da9d2a1937ec8e2bbb6b0887ec2c2a4ac4e3bb7
Andrew Li <andrewli@u.northwestern.edu>
2024-04-21 15:48:13 -0500

    update bio

--- src/Components/AboutContent.tsx
+++ src/Components/AboutContent.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const AboutContent: FC<{index: number, endpoint: string}> = ({index, endpoint}) => {
   return (
     <div style={{display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: index % 2 === 0 ? "flex-start" : "flex-end"}}>
-      <div>Hello! I'm Andrew (they/them), and I study integrated sciences, math, and computer science at Northwestern University. My interests are in machine learning and software development, and I also enjoy writing code for STEM research. I'm well-versed in both STEM and humanities fields, and I want to pursue a career in computer science.</div>
+      <div>I'm Andrew Li, and I major in <a href="https://isp.northwestern.edu/">Integrated Sciences</a>, Computer Science, and Chemistry at Northwestern University.</div>
     </div>
   );
 };
